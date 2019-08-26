e396755b8e9e9fbd5e3bd17ff24f4b8894d60aa1
Cao Trung Hieu（344552） <ty344552@toyo-local.co.jp>
2019-08-26 10:27:01 +0700

    merge by api

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var routes = require('./api/router');
+// require('dotenv').load()
+var model = require('./models');
+model.sequelize
+  .sync()
+  .then(function() {
+    //     console.log('nice');
+  })
+  ['catch'](function() {
+    //     console.log('not nice');
+  });
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+var port = process.env.PORT || 3001;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// let routes = require('./api/routes');
+routes(app);
+app.use(function(req, res) {
+  res.status(404).send({ url: req.originalUrl + ' not found' });
+});
+app.listen(port);
+console.log('RESTful API server started on: ' + port);
